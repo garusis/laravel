ef3ff83319409918e47b11d73e26342028491bcf
Marcos J. Alvarez <marcos.alvarez@rokk3rlabs.com>
2016-05-18 21:36:34 -0500

    Use Openshift as base

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(User::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Authenticates a User
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {          // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        return response()->json(compact('token'));
+    }
+}
---
+++ public/assets/js/scripts.js
@@ -0,0 +1,70 @@
+/**
+ * Created by garusis on 16/05/16.
+ */
+angular
+    .module('sw2', ['ngMaterial', 'satellizer', 'ui.router', 'ngMessages','md.data.table'])
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('default');
+    })
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider
+            .otherwise(function ($injector, $location) {
+                var $state = $injector.get('$state');
+                $state.go('home_calendars_list');
+            });
+
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'partials/users.login.html',
+                controller: 'LoginController',
+                controllerAs: 'loginCtrl'
+            })
+            .state('home', {
+                url: '/',
+                abstract: true
+            })
+            .state('home.calendars', {
+                url: 'calendarios/',
+                abstract: true
+            })
+            .state('home_calendars_list', {
+                url: '/calendarios/listado',
+                templateUrl: 'partials/calendars.list.html'
+            })
+            .state('home_calendars_new', {
+                url: '/calendarios/nuevo',
+                templateUrl: 'partials/calendars.new.html'
+            });
+    })
+    .config(function ($authProvider) {
+        $authProvider.loginUrl = document.getElementsByTagName('base')[0].href + 'index.php/api/authenticate';
+    })
+    .run(function ($rootScope, $auth, $state) {
+        $rootScope.$state = $state;
+
+        $rootScope.$on('$stateChangeStart', function (e, toState, toParams, fromState, fromParams) {
+            if (toState.name !== 'login' && !$auth.isAuthenticated()) {
+                e.preventDefault();
+                $state.go('login');
+            }
+        });
+    })
+    .controller('LoginController', function ($scope, $auth, $state) {
+        $scope.vmLogin = {};
+
+        this.login = function ($form, credentials) {
+            $auth
+                .login(credentials)
+                .then(function () {
+                    $state.go('home_calendars_list');
+                })
+                .catch(function () {
+                    $form.username.$setValidity('match', false);
+                });
+        }
+    })
+    .controller('HomeController', function ($scope) {
+
+    });
