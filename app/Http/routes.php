a36b78b07e22dab9360c09df2c752819a066f83d
Marcos J. Alvarez <marcos.alvarez@rokk3rlabs.com>
2016-05-19 00:33:16 -0500

    cambios

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
 
-Route::group(['prefix'=>'api'], function(){
-    Route::resource('users','UserController');
-    Route::post('authenticate', 'UserController@authenticate');
-});
+Route::get('/', 'WelcomeController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
